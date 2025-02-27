c69d4cdb9c0a5cb0dfa5eac45f5a77bbd960585e
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-27 13:38:27 +0600

    i just added here new functionality here for size buttons

--- js/app.js
+++ js/app.js
@@ -2,14 +2,29 @@
 const allRingButtons = document.querySelectorAll('.ring-button');
 for (let ringButtons of allRingButtons) {
     ringButtons.addEventListener('click', function (event) {
-        const color = event.target.id.replace('-color', '');
         for (let ringBtn of allRingButtons) {
             ringBtn.classList.remove('border-purple-700');
             ringBtn.classList.add('border-gray-100');
         }
         event.target.classList.add('border-purple-700');
         event.target.classList.remove('border-gray-100');
+        const color = event.target.id.replace('-color', '');
         const productImage = document.getElementById('product-image');
         productImage.src = '../images/' + color + '.png';
     })
+}
+
+// size button functionality 
+function selectWristSize(size) {
+    const sizes = ['S', 'M', 'L', 'XL'];
+    for (let siz of sizes) {
+        const button = document.getElementById('size-' + siz);
+        console.log(button);
+        if (size === siz) {
+            button.classList.add('border-purple-700');
+        }
+        else {
+            button.classList.remove('border-purple-700');
+        }
+    }
 }
